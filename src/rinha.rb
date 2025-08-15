fb0db47dee23ab87b71bf116e9d3a6e1f25854c8
Guilherme Silva <guilherme.gss@outlook.com.br>
2025-08-15 00:33:29 -0300

    aaaaaa :sob: :sob: :sob:

--- src/rinha.rb
+++ src/rinha.rb
@@ -51,23 +51,23 @@ class DefaultHealthcheck
     return if @checking
 
     @checking = true
-    Async do
-      begin
-        uri = URI.parse(@healthcheck_url)
-        http = Net::HTTP.new(uri.host, uri.port)
-        http.get('/payments/service-health') do |response|
-          @last_status = !JSON.parse(response)['failing']
-          @last_check_time = Process.clock_gettime(Process::CLOCK_MONOTONIC)
-          Console.logger.info("Healthcheck status: #{@last_status ? 'De boa' : 'Nao ta de boa'}  ")
-        end
-      rescue StandardError => e
-        @last_status = false
+    # Async do
+    begin
+      uri = URI.parse(@healthcheck_url)
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.get('/payments/service-health') do |response|
+        @last_status = !JSON.parse(response)['failing']
         @last_check_time = Process.clock_gettime(Process::CLOCK_MONOTONIC)
-        Console.logger.error("Healthcheck failed: #{e.message}")
+        Console.logger.info("Healthcheck status: #{@last_status ? 'De boa' : 'Nao ta de boa'}  ")
       end
-    ensure
-      @checking = false
+    rescue StandardError => e
+      @last_status = false
+      @last_check_time = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+      Console.logger.error("Healthcheck failed: #{e.message}")
     end
+    # ensure
+    # @checking = false
+    # end
   end
 end
 
@@ -83,13 +83,13 @@ class AsyncPaymentQueue
   end
 
   def start
-    Async do |task|
-      @max_workers.times do |i|
-        @workers << task.async do
-          worker_loop(i)
-        end
+    # Async do |task|
+    @max_workers.times do |i|
+      @workers << Async do
+        worker_loop(i)
       end
     end
+    # end
   end
 
   def enqueue(job)
@@ -100,17 +100,28 @@ class AsyncPaymentQueue
     ''
   end
 
+  def size
+    @queue.size
+  end
+
   private
 
   def worker_loop(worker_id)
     while @running
       begin
+        Console.logger.info 'aqui!'
         job = @queue.dequeue
-        fallback_job = @fallback_queue.dequeue
-        process_job(job, worker_id) if job
-        process_fallback(fallback_job, worker_id) if fallback_job
+        # fallback_job = @fallback_queue.dequeue
+        process_job(job, worker_id) # if job
+        Console.logger.info("Worker #{worker_id} pegou job: #{job.inspect}") if job
+        # process_fallback(fallback_job, worker_id) if fallback_job
+        unless @fallback_queue.empty?
+          fallback_job = @fallback_queue.dequeue
+          process_fallback(fallback_job, worker_id) if fallback_job
+          Console.logger.info("Worker #{worker_id} pegou fallback: #{fallback_job.inspect}") if fallback_job
+        end
       rescue StandardError => e
-        puts "Error processing job in worker #{worker_id}: #{e.message}"
+        Console.logger.info "Error processing job in worker #{worker_id}: #{e.message}"
         Async::Task.current.sleep(1)
       end
     end
@@ -124,63 +135,63 @@ class AsyncPaymentQueue
       return
     end
 
-    Async do
-      # endpoint = Async::HTTP::Endpoint.parse(DEFAULT_PROCESSOR_URL)
-      # client = Async::HTTP.Client.new(endpoint, retries: 2)
+    # Async do
+    # endpoint = Async::HTTP::Endpoint.parse(DEFAULT_PROCESSOR_URL)
+    # client = Async::HTTP.Client.new(endpoint, retries: 2)
 
-      # response = client.post("/payments", [["content-type", "application/json"]], job[:data])
-      # if response.ok?
-      #   Console.logger.info("Job #{job} processado com sucesso pelo worker #{worker_id}")
-      #   client&.close
-      # end
-      uri = URI.parse(DEFAULT_PROCESSOR_URL)
-      http = Net::HTTP.new(uri.host, uri.port)
-      http.max_retries = 2
-      req = Net::HTTP::Post.new('/payments')
-      req['Content-Type'] = 'application/json'
-      req.body = job.to_json
+    # response = client.post("/payments", [["content-type", "application/json"]], job[:data])
+    # if response.ok?
+    #   Console.logger.info("Job #{job} processado com sucesso pelo worker #{worker_id}")
+    #   client&.close
+    # end
+    uri = URI.parse(DEFAULT_PROCESSOR_URL)
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.max_retries = 2
+    req = Net::HTTP::Post.new('/payments')
+    req['Content-Type'] = 'application/json'
+    req.body = job.to_json
 
-      http.request(req) do |response|
-        if response.is_a?(Net::HTTPSuccess)
-          Console.logger.info("Job #{job} processado com sucesso pelo worker #{worker_id}")
-          @session.call("INSERT INTO payments (correlationId, amount, requested_at, p) VALUES('#{job[:correlationId]}', #{job[:amount]}, '#{job[:requestedAt]}', 'default')")
-        else
-          Console.logger.info("Erro ao processar job #{job} pelo worker #{worker_id}: #{response.message}")
-          retry_job(job, 'fallback')
-        end
+    http.request(req) do |response|
+      if response.is_a?(Net::HTTPSuccess)
+        Console.logger.info("Job #{job} processado com sucesso pelo worker #{worker_id} na rota: #{uri}")
+        @session.call("INSERT INTO payments (correlationId, amount, requested_at, p) VALUES('#{job[:correlationId]}', #{job[:amount]}, '#{job[:requestedAt]}', 'default')")
+      else
+        Console.logger.info("Erro ao processar job #{job} pelo worker #{worker_id}: #{response.message}")
+        retry_job(job, 'fallback')
       end
-    rescue StandardError => e
-      Console.logger.error("Error processing job #{job} in worker #{worker_id}: #{e.message}")
-      retry_job(job, 'default')
-    ensure
-      @session.close
     end
+  rescue StandardError => e
+    Console.logger.info("Error processing job #{job} in worker #{worker_id}: #{e.message}")
+    retry_job(job, 'default')
+    # ensure
+    #  @session.close
+    # end
   end
 
   def process_fallback(job, worker_id)
-    Async do
-      uri = URI.parse(FALLBACK_PROCESSOR_URL)
-      http = Net::HTTP.new(uri.host, uri.port)
-      http.max_retries = 2
-      req = Net::HTTP::Post.new('/payments')
-      req['Content-Type'] = 'application/json'
-      req.body = job.to_json
+    # Async do
+    uri = URI.parse(FALLBACK_PROCESSOR_URL)
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.max_retries = 2
+    req = Net::HTTP::Post.new('/payments')
+    req['Content-Type'] = 'application/json'
+    req.body = job.to_json
 
-      http.request(req) do |response|
-        if response.is_a?(Net::HTTPSuccess)
-          Console.logger.info("Job #{job} processado com sucesso pelo worker #{worker_id}")
-          @session.call("INSERT INTO payments (correlationId, amount, requested_at, p) VALUES('#{job[:correlationId]}', #{job[:amount]}, '#{job[:requestedAt]}', 'default')")
-        else
-          Console.logger.info("Erro ao processar job #{job} pelo worker #{worker_id}: #{response.message}")
-          retry_job(job, 'fallback')
-        end
+    http.request(req) do |response|
+      if response.is_a?(Net::HTTPSuccess)
+        Console.logger.info("Job #{job} processado com sucesso pelo worker #{worker_id}")
+        @session.call("INSERT INTO payments (correlationId, amount, requested_at, p) VALUES('#{job[:correlationId]}', #{job[:amount]}, '#{job[:requestedAt]}', 'default')")
+      else
+        Console.logger.info("Erro ao processar job #{job} pelo worker #{worker_id}: #{response.message} no fallback")
+        retry_job(job, 'fallback')
       end
-    rescue StandardError => e
-      Console.logger.error("Error processing job #{job} in worker #{worker_id}: #{e.message}")
-      retry_job(job, 'default')
-    ensure
-      @session.close
     end
+  rescue StandardError => e
+    Console.logger.info("Error processing job #{job} in worker #{worker_id}: #{e.message}")
+    retry_job(job, 'default')
+    # ensure
+    #  @session.close
+    # end
   end
 
   def retry_job(job, processor)
@@ -192,10 +203,13 @@ end
 
 session = CLIENT.session
 HEALTHCHECK = DefaultHealthcheck.new(check_interval: 5)
-payment_queue = AsyncPaymentQueue.new(max_workers: 10, healthcheck: HEALTHCHECK, session: session)
+PAYMENT_QUEUE = AsyncPaymentQueue.new(max_workers: 10, healthcheck: HEALTHCHECK, session: session)
 
 Async do
-  payment_queue.start
+  Console.logger.info 'antes do start'
+  PAYMENT_QUEUE.start
+  Console.logger.info 'depois do start'
+  Console.logger.info "#{PAYMENT_QUEUE.size} jobs enfileirados"
 
   Guaraci::Server.run(host: '0.0.0.0', port: 3000) do |request|
     case [request.method, request.path_segments]
@@ -206,14 +220,51 @@ Async do
         requestedAt: Time.now.iso8601
       }
 
-      payment_queue.enqueue(payment_data)
+      PAYMENT_QUEUE.enqueue(payment_data)
+      Console.logger.info("Job enfileirado: #{PAYMENT_QUEUE.size}")
       Guaraci::Response.ok.render
+    in ['GET', [rest]]
+      handle_summary(rest)
+    else
+      Guaraci::Response.ok.json({ message: 'nada' }).render
     end
   end
-end
 
-# executor = proc do |job|
-#   NET::HTTP.start("0.0.0.0", 3000) do |http|
-#     http.post("/payments", job)
-#   end
-# end
+  def handle_summary(path)
+    params = path.split('?')[1].split('&')&.map { |q| q.split('=') }.to_a
+    session = CLIENT.session
+    unless params.empty?
+      from = params[0][1]
+      to = params[1][1]
+      Console.logger.info("Agregando resumo dos pagamentos, de: #{from}, ate: #{to}")
+      result = session.call("SELECT COUNT(*) as total_p, SUM(amount) as a, p as p FROM payments WHERE requested_at BETWEEN '#{from}' AND '#{to}' GROUP BY p")
+      Console.logger.info("Resultado do resumo: #{result.to_a.inspect}")
+
+      return Guaraci::Response.ok.json({
+                                         default: {
+                                           totalRequests: result.to_a[0][1],
+                                           totalAmount: result.to_a[0][2]
+                                         },
+                                         fallback: {
+                                           totalRequests: result.to_a[1][1],
+                                           totalAmount: result.to_a[1][2]
+                                         }
+                                       }).render if result.to_a.any?
+    end
+
+    result = session.call("SELECT COUNT(*) as total_p, SUM(amount) as a, p as p FROM payments WHERE requested_at BETWEEN '#{from}' AND '#{to}' GROUP BY p")
+    Console.logger.info("Resultado do resumo: #{result.to_a.inspect} fora do unless")
+# "Resultado do resumo: [[366, 0.72834e4, \"default\"]]
+## se nao tiver fallback no banco resolver
+    Guaraci::Response.ok.json({
+                                default: {
+                                  totalRequests: result.to_a[0][0],
+                                  totalAmount: result.to_a[0][1]
+                                },
+                                fallback: {
+                                  totalRequests: result.to_a[1][0],
+                                  totalAmount: result.to_a[1][1]
+                                }
+                              }).render if result.to_a.any?
+  end
+end
